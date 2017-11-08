c609006e0ecc3ab018807a55c45e83693439f1f5
maxchehab <maxchehab@gmail.com>
2017-11-07 17:40:49 -0800

    Implemented image and link redirect

--- main.go
+++ main.go
@@ -7,36 +7,16 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, r.URL.Query().Get("v"))
-	fmt.Print(formatRequest(r))
+	var userAgent = r.Header.Get("User-Agent")
+	if strings.Contains(userAgent, "github-camo") {
+		http.Redirect(w, r, "https://img.youtube.com/vi/"+r.URL.Query().Get("v")+"/0.jpg", 200)
+	} else {
+		http.Redirect(w, r, "https://youtube.com/watch?v="+r.URL.Query().Get("v"), 200)
+	}
 }
 
 func main() {
+	fmt.Print("> Listening on http://localhost:8080\n")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
-}
-
-func formatRequest(r *http.Request) string {
-	var request []string
-
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
-
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-
-	}
-	return strings.Join(request, "\n")
-
 }
